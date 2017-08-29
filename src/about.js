d4760372fd7764b478980e13af1963d1d7e26644
burkeboston1 <bburke96@vt.edu>
2017-08-28 23:22:00 -0400

    about section

--- src/about.js
+++ src/about.js
@@ -13,7 +13,14 @@ export default class About extends Component {
        			<div className="w3-container w3-half w3-margin cp-white">
        				<h2 className="font-cabinsketch w3-center"><strong>Crawford and Power</strong></h2>
        				<h4 className="font-arvo"> 
-       					This is a paragraph about us and stuff.
+       					After meeting in the early months of 2014 in college, Jacob Crawford and Ethan Power
+						realized they shared a common ground and similar tastes in music. The two soon realized
+						this common ground was unlike any project they had ever been in before. After opening 
+						for acts like Jerry Douglas and Chris Knight not long after just meeting one another, 
+						they decided to form their duo, Crawford & Power. The duo have since been gigging around 
+						Southwest Virginia and have shared the stage with acts such as Luke Combs, Drake White, 
+						and Confederate Railroad. They are currently focusing on writing and recording for their 
+						debut release. 
        				</h4>
        			</div>
             </div>
